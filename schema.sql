77e123831a71ac1a6836f4878d818471ec7017ce
otsotilus <otsotilus@gmail.com>
2024-11-07 15:56:17 +0200

    projekti alustettu

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE if exists comments;
+DROP TABLE if exists posts;
+DROP TABLE if exists users;
+
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    breadtext TEXT NOT NULL,
+    youtube_url VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    pw VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    comment TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
