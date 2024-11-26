19f2436c1ff9f7ea0bebc5dffdbc620fb1383e74
otsotilus <otsotilus@gmail.com>
2024-11-26 13:56:30 +0200

    Tietokantahaku järkeistetty

--- posts.py
+++ posts.py
@@ -4,11 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from db import db
 from sqlalchemy import text
 
+#def get_list():
+#    sql = text(
+#      "SELECT  P.id, P.body, P.youtube_url, P.created_at\
+#      FROM posts P, users U \
+#      WHERE P.user_id=U.id \
+#      ORDER BY P.id")
+#    result = db.session.execute(sql)
+#    return result.fetchall()
+
 def get_list():
-    sql = text(
-      "SELECT  P.id, P.body, P.youtube_url, P.created_at\
-      FROM posts P, users U \
-      WHERE P.user_id=U.id \
-      ORDER BY P.id")
-    result = db.session.execute(sql)
-    return result.fetchall()
+  sql = text("""SELECT P.id,
+             P.body,
+             P.youtube_url,
+             P.created_at,
+             ARRAY_AGG(array[C.id::varchar(255), C.comment]) AS comments
+             FROM posts AS P
+             JOIN users AS U ON P.user_id = U.id
+             LEFT JOIN comments AS C ON C.post_id=P.id
+             WHERE P.user_id=U.id
+             GROUP BY P.id
+             ORDER BY P.id
+             """)
+  posts = db.session.execute(sql)
+  return posts.fetchall()
